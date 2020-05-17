d3cea1414aafebe3d8d6cb0bc4c273190720485e
RiedelNicolas <nariedel99@gmail.com>
2020-05-17 15:24:34 -0300

    pasa a enteros correctamente

--- main.c
+++ main.c
@@ -12,12 +12,11 @@
 POST: Lee una linea de dicho stream y devuelve la misma en forma de array de caracteres. Tambien le asigna a largo la cantidad de caracteres que leyo
 */
 
-char* leer (FILE* stream);
-int* pasar_a_enteros(char* linea, int* largo_enteros);
+char* leer (FILE* stream, int *largo_linea);
+int* pasar_a_enteros(char* linea, int largo_linea, int* largo_enteros);
 bool es_fin_de_linea(char caracter);
 bool es_numerico(char caracter);
 
-void linea_a_string(char* linea);
 
 int main(){
 
@@ -25,15 +24,13 @@ int main(){
 	FILE* stream_entrada = NULL;
 	int *enteros = NULL;
 	int largo_enteros = 0;
+	int largo_linea = 0;
 
 	stream_entrada = fopen("tests/entrada.txt", "r");
 
-	linea = leer(stream_entrada);
+	linea = leer(stream_entrada, &largo_linea);
 
-	linea_a_string(linea);
-	printf("%s",linea);
-
-	enteros = pasar_a_enteros(linea, &largo_enteros);
+	enteros = pasar_a_enteros(linea, largo_linea, &largo_enteros);
 
 	printf("%i",largo_enteros);
 		
@@ -45,41 +42,29 @@ int main(){
 
 
 
-char* leer(FILE* stream ){
+char* leer(FILE* stream, int *largo_linea){
 
 	int largo_buffer = 20;
 	char *linea = (char*) malloc(sizeof(char) * largo_buffer); // Asigno un lugar en memoria para el linea.
-	int largo_linea = 0;
+	(*largo_linea) = 0;
 	char caracter = 'A'; // un valor trivial
 
 	while ( (caracter != EOL ) && (caracter != EOF) ) {
 
-    	if( largo_linea == (largo_buffer-1) ){ // tengo que agrandar mi memoria.(Dejo lugar para /0)
+    	if( (*largo_linea) == (largo_buffer-1) ){ // tengo que agrandar mi memoria.(Dejo lugar para /0)
      	 	largo_buffer +=10; //Voy agregando de a 10 lugares. 
       		linea = realloc(linea, largo_buffer); // re ubico en la memoria.
     	}
 
     caracter = getc(stream); // Leo un caracter del stream.
-    linea[largo_linea] = caracter; //Lo guardo en el linea.
-    (largo_linea)+=1; //Incremento mi tope.
+    linea[ (*largo_linea) ] = caracter; //Lo guardo en el linea.
+    (*largo_linea)+=1; //Incremento mi tope.
 	}
 
 	
 	return linea;
 }
 
-void linea_a_string(char* linea){
-
-	int i = 0;
-
-	while(1){
-		if( es_fin_de_linea(linea[i]) ){
-			linea[i] = '\0';
-			return;	
-		}
-		i++;
-	}
-}
 
 bool es_fin_de_linea(char caracter){
 
@@ -87,7 +72,7 @@ bool es_fin_de_linea(char caracter){
 }
 
 
-int* pasar_a_enteros(char* linea, int* largo_enteros){
+int* pasar_a_enteros(char* linea, int largo_linea, int* largo_enteros){
 	char temporal [MAX_DIGITOS];
 	char caracter = 'A';
 	int largo_buffer = 10;
@@ -98,7 +83,7 @@ int* pasar_a_enteros(char* linea, int* largo_enteros){
 	int i = 0;
  	int j = 0;
 
-	while( caracter != '\0' ){
+	while( i<largo_linea ){
 
 		if( (*largo_enteros) == (largo_buffer-1) ){
 			largo_buffer +=10; //Voy agregando de a 10 lugares. 
@@ -109,7 +94,7 @@ int* pasar_a_enteros(char* linea, int* largo_enteros){
 		if( es_numerico(caracter) ){
 			temporal[j] = caracter; j++;	
 		}
-		else if(caracter =' '){
+		else if( (caracter =' ' || es_fin_de_linea(caracter) ) && j!=0 ){
 			temporal[j] = '\0';
 			enteros[(*largo_enteros)] = atoi(temporal);
 			(*largo_enteros) += 1;
