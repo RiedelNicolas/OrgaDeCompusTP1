179d12b9370de913137d637150cce4f4f1f1606a
RiedelNicolas <nariedel99@gmail.com>
2020-05-18 02:50:19 -0300

    andamio

--- main.c
+++ main.c
@@ -11,16 +11,15 @@
 
 int main(){
 
-	char* linea = NULL;
+
 	FILE* stream_entrada = NULL;
 	FILE* stream_salida = NULL;
-	int *enteros = NULL;
-	int largo_enteros = 0;
-	int largo_linea = 0;
 
 	stream_entrada = fopen("tests/entrada.txt", "r");
 	stream_salida = fopen("tests/salida.txt", "w");
+
 	ordenar(stream_entrada, stream_salida);
+	
 	fclose(stream_entrada);
 	fclose(stream_salida);
 	return 0;
--- ordenador.c
+++ ordenador.c
@@ -3,12 +3,14 @@
 #define MAX_PATH 150
 #define EOL '\n'
 #define MAX_DIGITOS 20
-
+#define FLAG_FIN_DE_ARCHIVO 1 
+#define FLAG_LINEA_INVALIDA -1
+#define FLAG_CONTINUAR 0
 
 
 
 void ordenar(FILE* entrada, FILE* salida);
-char* leer (FILE* stream, int *largo_linea);
+int leer (FILE* stream, int *largo_linea, char** linea);
 int* pasar_a_enteros(char* linea, int largo_linea, int* largo_enteros);
 bool es_fin_de_linea(char caracter);
 bool es_numerico(char caracter);
@@ -18,27 +20,34 @@ void imprimir_enteros(int *enteros, size_t largo, FILE* salida);
 
 void ordenar(FILE* entrada, FILE* salida){
 
-	char* linea = NULL;
+	int flag_lectura = FLAG_CONTINUAR ;
 	int *enteros = NULL;
 	int largo_enteros = 0;
 	int largo_linea = 0;
+	char* linea = NULL;
 
-	linea = leer(entrada, &largo_linea);
+	while(flag_lectura == FLAG_CONTINUAR){
 
-	enteros = pasar_a_enteros(linea, largo_linea, &largo_enteros);
-
-	merge_sort(enteros, (size_t) largo_enteros);
-
-	imprimir_enteros(enteros, largo_enteros, salida);
+		flag_lectura = leer(entrada, &largo_linea, &linea);
 		
-	free(linea);
-	free(enteros);
+		if(flag_lectura !=FLAG_LINEA_INVALIDA ){
+
+			enteros = pasar_a_enteros(linea, largo_linea, &largo_enteros);
+			merge_sort(enteros, (size_t) largo_enteros);
+			imprimir_enteros(enteros, largo_enteros, salida); 
+			free(enteros);
+		}
+
+		free(linea);
+	}
+	
 }
 
-char* leer(FILE* stream, int *largo_linea){
+
+ int leer(FILE* stream, int *largo_linea, char** linea){
 
 	int largo_buffer = 20;
-	char *linea = (char*) malloc(sizeof(char) * largo_buffer); // Asigno un lugar en memoria para el linea.
+	*linea = (char*) malloc(sizeof(char) * largo_buffer); // Asigno un lugar en memoria para el linea.
 	(*largo_linea) = 0;
 	char caracter = 'A'; // un valor trivial
 
@@ -46,22 +55,34 @@ char* leer(FILE* stream, int *largo_linea){
 
     	if( (*largo_linea) == (largo_buffer-1) ){ // tengo que agrandar mi memoria.(Dejo lugar para /0)
      	 	largo_buffer +=10; //Voy agregando de a 10 lugares. 
-      		linea = realloc(linea, largo_buffer); // re ubico en la memoria.
+      		(*linea) = (char*) realloc((*linea), sizeof(char) * largo_buffer); // re ubico en la memoria.
     	}
 
-    caracter = getc(stream); // Leo un caracter del stream.
-    linea[ (*largo_linea) ] = caracter; //Lo guardo en el linea.
-    (*largo_linea)+=1; //Incremento mi tope.
+    	caracter = getc(stream); // Leo un caracter del stream.
+    	(*linea) [ (*largo_linea) ] = caracter; //Lo guardo en el linea.
+    	(*largo_linea)+=1; //Incremento mi tope.
 	}
 
-	
-	return linea;
+
+
+
+
+	if( (*largo_linea) <=1 ){     // Siempre va a leer por lo menos un caracter, sea eof o fin de linea
+		return FLAG_LINEA_INVALIDA;
+	}
+
+	if(caracter == EOF){
+		return FLAG_FIN_DE_ARCHIVO;
+	}
+
+
+	return 	FLAG_CONTINUAR;
+		
 }
 
 
-
-
 int* pasar_a_enteros(char* linea, int largo_linea, int* largo_enteros){
+
 	char temporal [MAX_DIGITOS];
 	char caracter = 'A';
 	int largo_buffer = 10;
@@ -76,7 +97,7 @@ int* pasar_a_enteros(char* linea, int largo_linea, int* largo_enteros){
 
 		if( (*largo_enteros) == (largo_buffer-1) ){
 			largo_buffer +=10; //Voy agregando de a 10 lugares. 
-      		enteros = realloc(enteros, largo_buffer); // re ubico en la memoria.
+      		enteros = (int*) realloc(enteros, sizeof(int)* largo_buffer); // re ubico en la memoria.
 		}
 
 		caracter = linea[i]; i++;
@@ -93,15 +114,12 @@ int* pasar_a_enteros(char* linea, int largo_linea, int* largo_enteros){
 	}
 
 	return enteros;
-
 }
-
 
 bool es_fin_de_linea(char caracter){
 
 	return(caracter == EOF || caracter == EOL);
 }
-
 
 bool es_numerico(char caracter){
 	return(caracter >= '0' && caracter <= '9' );
