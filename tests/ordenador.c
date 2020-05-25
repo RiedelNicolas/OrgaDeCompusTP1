9bf5ef39dcae7de65de15f4239b115fba204a763
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2020-05-25 15:11:15 -0300

    Corrijo merge_sort.S

--- tests/ordenador.c
+++ tests/ordenador.c
@@ -33,7 +33,7 @@ void ordenar(FILE* entrada, FILE* salida){
 		if(flag_lectura !=FLAG_LINEA_INVALIDA ){
 
 			enteros = pasar_a_enteros(linea, largo_linea, &largo_enteros);
-			merge_sort(enteros, 0, (size_t) largo_enteros-1);
+			merge_sort(enteros,(size_t) largo_enteros);
 			imprimir_enteros(enteros, largo_enteros, salida);
 			free(enteros);
 		}
@@ -126,63 +126,63 @@ bool es_numerico(char caracter){
 }
 
 // Merge two subarrays L and M into arr
-void merge(int arr[], size_t p, size_t q, size_t r) {
-
-  // Create L ← A[p..q] and M ← A[q+1..r]
-  int n1 = q - p + 1;
-  int n2 = r - q;
-
-  int L[n1], M[n2];
-
-  for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
-  for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
-
-  // Maintain current index of sub-arrays and main array
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
-
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
-    }
-    k++;
-  }
-
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
-}
-void merge_sort(int *vec, size_t inicio, size_t fin){
-
-  if(inicio<fin){
-    int medio = (inicio+fin-1)/2;
-
-    merge_sort(vec, inicio, medio);
-    merge_sort(vec, medio+1, fin);
-
-    merge(vec, inicio, medio, fin);
-  }
-}
+// void merge(int arr[], size_t p, size_t q, size_t r) {
+//
+//   // Create L ← A[p..q] and M ← A[q+1..r]
+//   int n1 = q - p + 1;
+//   int n2 = r - q;
+//
+//   int L[n1], M[n2];
+//
+//   for (int i = 0; i < n1; i++)
+//     L[i] = arr[p + i];
+//   for (int j = 0; j < n2; j++)
+//     M[j] = arr[q + 1 + j];
+//
+//   // Maintain current index of sub-arrays and main array
+//   int i, j, k;
+//   i = 0;
+//   j = 0;
+//   k = p;
+//
+//   // Until we reach either end of either L or M, pick larger among
+//   // elements L and M and place them in the correct position at A[p..r]
+//   while (i < n1 && j < n2) {
+//     if (L[i] <= M[j]) {
+//       arr[k] = L[i];
+//       i++;
+//     } else {
+//       arr[k] = M[j];
+//       j++;
+//     }
+//     k++;
+//   }
+//
+//   // When we run out of elements in either L or M,
+//   // pick up the remaining elements and put in A[p..r]
+//   while (i < n1) {
+//     arr[k] = L[i];
+//     i++;
+//     k++;
+//   }
+//
+//   while (j < n2) {
+//     arr[k] = M[j];
+//     j++;
+//     k++;
+//   }
+// }
+// void merge_sort(int *vec, size_t inicio, size_t fin){
+//
+//   if(inicio<fin){
+//     int medio = (inicio+fin-1)/2;
+//
+//     merge_sort(vec, inicio, medio);
+//     merge_sort(vec, medio+1, fin);
+//
+//     merge(vec, inicio, medio, fin);
+//   }
+// }
 
 void imprimir_enteros(int *enteros, size_t largo, FILE* salida){
 	for (int i = 0; i < largo; i++){
